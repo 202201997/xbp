159347f7cc019adf7d8a91360fdb7a7a96bf26e8
202201997 <r202201997ur@jindai.jp>
2022-10-31 16:14:27 +0900

    20221031

---
+++ de12/python/kadai1.py
@@ -0,0 +1,39 @@
+print("あなたは今、約束を守らず、親に怒られている！"
+"もし今回も下手なことを言ったら、追い出されるかもしれない。"
+"さあなんて言おう...")
+name=input("まず、あなたのお名前は？")
+
+selecta = int(input("運命の数字1～3の中から選んでね！"))
+if selecta ==1:
+    print("反抗期がきた！")
+
+elif selecta == 3:
+    print("人のせいにする！")
+
+elif selecta == 2:
+    print("理由を正直に言って、反省した顔をする")
+
+print("母は詳しい事情を聞こうとしてきた。でもちょっと言いづらい...
+selectb = int(input("1~3の中から選んでね！"))
+if selectb == 1: 
+    print("話をずらし、ごまかす！")
+elif selectb == 2:
+    print("事情は詳しく話せないから無視だ！")
+
+elif selectb == 3:
+    print("嘘と本当のハイブリット型！")
+
+print("母はなんとか納得し、今後どう行動すればいいのか聞いてきた。")
+
+selectc = int(input("運命の数字1～3の中から選んでね！"))
+if selectc == 1:
+    print("だるいからとりあえずごまかす")
+
+elif selectc == 2:
+    print("３時間かけて計画書を作成し、提出する。")
+
+elif selectc ==3:
+    print("もうよくわからないから一緒に考える")
+
+ 
+print("なんとか乗り過ごせた！ナイス！")   
---
+++ de12/python/work1.py
@@ -0,0 +1,7 @@
+name="abc"
+waist=55
+age=32
+height=170
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才、","身長は",height,"cmですね。")
+# aaaaaaaa
